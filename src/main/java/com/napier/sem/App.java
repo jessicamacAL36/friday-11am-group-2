7e90dbed13130035da933cdd4d52a92ded647743
jessicamacAL36 <40714782@live.napier.ac.uk>
2025-10-01 16:03:16 +0100

    new updates again

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,9 +1,41 @@
 package com.napier.sem;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+
 public class App
 {
     public static void main(String[] args)
     {
-        System.out.println("Boo yah!");
+        // === IMPLEMENT TRY-WITH-RESOURCES HERE ===
+        try (MongoClient mongoClient = new MongoClient("localhost", 27000))
+        {
+            // Get a database - will create when we use it
+            MongoDatabase database = mongoClient.getDatabase("mydb");
+
+            // Get a collection from the database
+            MongoCollection<Document> collection = database.getCollection("test");
+
+            // Create a document to store
+            Document doc = new Document("name", "Kevin Sim")
+                    .append("class", "DevOps")
+                    .append("year", "2024")
+                    .append("result", new Document("CW", 95).append("EX", 85));
+
+            // Add document to collection
+            collection.insertOne(doc);
+
+            // Check document in collection
+            Document myDoc = collection.find().first();
+            System.out.println(myDoc.toJson());
+        }
+        // Catch any exception that occurs during connection or operation
+        catch (Exception e)
+        {
+            System.err.println("An error occurred during MongoDB operation: " + e.getMessage());
+        }
+        // NOTE: The 'finally' block is no longer needed!
     }
 }
