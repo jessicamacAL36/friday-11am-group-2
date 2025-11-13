aabb02c13b8ae911f2dce675fd8c40bd01a95bf7
jessicamacAL36 <40714782@live.napier.ac.uk>
2025-11-13 14:47:34 +0000

    adding j unit

---
+++ src/test/java/com/napier/sem/AppTest.java
@@ -0,0 +1,13 @@
+package com.napier.sem;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+
+    // Test to ensure JUnit is working within the IDE
+    @Test
+    void isJUnitWorking() {
+        assertEquals(2 + 2, 4, "2 + 2 should equal 4");
+    }
+}
